8837c6e046e9378ca371b7a24334e93c8c103e32
Ryan Thally <thally@etsu.edu>
2019-10-16 00:08:58 -0400

    no underline yo

--- metricsui/src/components/ProjectCard.js
+++ metricsui/src/components/ProjectCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Link
+  Link as RouterLink
 } from 'react-router-dom'
 
 import {
@@ -9,7 +9,9 @@ import {
     CardActionArea,
     CardContent,
     Button,
-    Typography
+    Typography,
+    ButtonBase,
+    Link
 } from '@material-ui/core'
 
 const useStyles = makeStyles({
@@ -35,19 +37,21 @@ export default function ProjectCard(props) {
   const link = React.forwardRef((id, ref) => (
     <RouterLink innerRef={ref} to={"/projects/"+id+"/"} {...props} />
   ))
-  
+
   return (
-    <Card className={classes.card}>
-      <CardActionArea component={link}>
-        <CardContent>
-          <Typography variant="h5" component="h2" >
-            {name}
-          </Typography>
-          <Typography className={classes.title} color="textSecondary" gutterBottom>
-            {description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Link to="/about" component={RouterLink} underline='none'>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardContent>
+            <Typography variant="h5" component="h2" >
+              {name}
+            </Typography>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 }
