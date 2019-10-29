639324f549230be36d42cb3ed0b80531728e6d3c
Ryan Thally <rthally94@gmail.com>
2019-10-29 17:20:16 -0400

    cleanup

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,13 +1,7 @@
 class UserApi {
     static logInUser(loginParams) {
-<<<<<<< HEAD:metricsui/src/api/userApi.js
-        const {username, password} = loginParams;
-        console.log(`API: ${username} | ${password}`);
-        return fetch('login/', {
-=======
         const {username, password} = loginParams
         return fetch('/login/', {
->>>>>>> 2e2ecf698f33a3b3302f80516213af839b681c37:src/api/userApi.js
             method: 'POST',
             body: JSON.stringify({
               username: username,
@@ -17,19 +11,11 @@ class UserApi {
               'Content-Type': 'application/json'
             }
           })
-<<<<<<< HEAD:metricsui/src/api/userApi.js
-          .then(response => {
-              console.log(response);
-              return (
-              { username: username, token: response.json().token }
-              )
-=======
           .then(response => response.json()) 
           .then(data => {
             return (
               {username: username, token: data.token}
             )
->>>>>>> 2e2ecf698f33a3b3302f80516213af839b681c37:src/api/userApi.js
           })
           .catch(error => { return error })
     }
