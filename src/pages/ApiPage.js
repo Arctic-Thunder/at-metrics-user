9737beb5bbbcbe6b902d011577b261ca312e11cb
Colin Kelley <kelleycd@etsu.edu>
2019-11-05 17:46:54 -0500

    added gitignore to keep dependency modules from being added to the repo. Also updated api page.

--- src/pages/ApiPage.js
+++ src/pages/ApiPage.js
@@ -11,7 +11,7 @@ export default function ApiPage() {
     const apidata = [
         {name: "/api/new_user/", description: "POST: Create User"},
         {name: "/api/login/",description: "POST: Login User" },
-        {name: "/api/projects/{{project_id}}", description: "GET: Get Project ; DEL: Delete Project"},
+        {name: "/api/projects/{{project_id}}/", description: "GET: Get Project ; DEL: Delete Project"},
         {name: "/api/projects/{{project_id}}/metrics", description: "GET: Get All Metrics For Project ; POST: Add New Metric To Project"},
         {name: "/api/projects/{{project_id}}/metrics/{{metric_id}}/", description: "GET: Get Metric ; DEL: Delete Metric"},
     ]
