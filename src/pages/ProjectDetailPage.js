3c30a3dacdf95e0929c0007ed29eab2b50988226
Ryan Thally <thally@etsu.edu>
2019-11-09 14:57:35 -0500

    Fixed Table

--- src/pages/ProjectDetailPage.js
+++ src/pages/ProjectDetailPage.js
@@ -1,18 +1,57 @@
-import React, { useEffect } from 'react'
+import React, { forwardRef, useEffect } from 'react'
 import {
     Typography,
+    Divider,
+    Grid,
 } from '@material-ui/core'
+
+import {
+    AddBox,
+    ArrowUpward,
+    Check,
+    ChevronLeft,
+    ChevronRight,
+    Clear,
+    DeleteOutline,
+    Edit,
+    FilterList,
+    FirstPage,
+    LastPage,
+    Remove,
+    SaveAlt,
+    Search,
+    ViewColumn,
+} from '@material-ui/icons';
 
 import MaterialTable from 'material-table'
 
-import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getProject as getProjectAction } from '../actions/projectActions'
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    }
 
 export const ProjectDetailPage = (props) => {
     const { isAuthenticated } = props
 
-    useEffect( () => {
+    useEffect( (myProps) => {
         if ( props.project === undefined ) {
             props.getProject(props.project_id)
         }
@@ -22,20 +61,50 @@ export const ProjectDetailPage = (props) => {
 
     return (
         <section className="project-detail">
-            <Typography variant='h4'>{name}</Typography>
-            <Typography variant='body1'>
-                {description}
-            </Typography>
-            
-            <MaterialTable
-                columns={[
-                    { title: 'Time', field: 'time', type: 'datetime'},
-                    { title: 'Name', field: 'name'},
-                    { title: 'Description', field: 'desc'}
-                ]}
-                data={[{time: Date() , name: "Name", desc: "Description of row"}]}
-                title="Project 1 Metrics"
-            />
+            <Grid
+                container
+                spacing={4}
+                direction="column"
+                justify="flex-start"
+            >
+                <Grid item>
+                    <Typography variant='h4'>{name}</Typography>
+                    <Typography variant='body1'>
+                        {description}
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Divider />
+                </Grid>
+                <Grid item>
+                    <MaterialTable
+                        title={`Metrics`}
+                        columns={[
+                            { title: 'Time', field: 'time', type: 'datetime'},
+                            { title: 'Name', field: 'name'},
+                            { title: 'Description', field: 'desc'}
+                        ]}
+                        data={[{time: Date() , name: "Name", desc: "Description of row"}]}
+                        options={
+                            { search: false }
+                        }
+                        detailPanel={[
+                            {
+                                tooltip: 'Metric Details',
+                                render: rowData => {
+                                    return (
+                                        <div>
+                                            <Typography>Detail View</Typography>
+                                        </div>
+                                    )
+                                }
+                            }
+                        ]}
+                        onRowClick={(event, rowData, togglePanel) => togglePanel()}
+                        icons={tableIcons}
+                    />
+                </Grid>
+            </Grid>
         </section>
     )
 }
