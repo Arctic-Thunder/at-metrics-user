eb4a6d906c17bacf1a464e053994dd8855b031e8
Chris Harris <ioschrisharris@gmail.com>
2019-11-09 23:12:56 -0500

    And then there was a graph... (React-Vis implementation)

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -2,8 +2,21 @@ import React from 'react'
 import { Typography, Divider } from "@material-ui/core"
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import {XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, LineSeries} from 'react-vis';
 
 export const DashboardPage = (props) => {
+    const data = [
+        {x: 0, y: 8},
+        {x: 1, y: 5},
+        {x: 2, y: 4},
+        {x: 3, y: 9},
+        {x: 4, y: 1},
+        {x: 5, y: 7},
+        {x: 6, y: 6},
+        {x: 7, y: 3},
+        {x: 8, y: 2},
+        {x: 9, y: 0}
+        ];
     const renderRedirect = () => {
         if(!props.isAuthenticated) {
             console.log("Redirect Loaded")
@@ -17,6 +30,14 @@ export const DashboardPage = (props) => {
             <Typography variant='h5' align="left">Your Dashboard</Typography>
             <br />
             <Divider />
+            <br />
+            <XYPlot height={300} width= {300}>
+            <VerticalGridLines />
+            <HorizontalGridLines />
+            <XAxis />
+            <YAxis />
+            <LineSeries data={data} />
+            </XYPlot>
         </section>
     )
 }
