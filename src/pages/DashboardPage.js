287596241be8d85b153c6b0fb8e3ba535557b0c0
Chris Harris <ioschrisharris@gmail.com>
2019-11-11 14:32:40 -0500

    Added legend for better visualization

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -4,9 +4,17 @@ import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { setCurrentPage as setCurrentPageAction } from '../actions/pageChangeActions'
 import { RadialChart, VerticalGridLines, XAxis, YAxis, XYPlot, HorizontalGridLines, LineSeries } from 'react-vis';
+import DiscreteColorLegend from 'react-vis/dist/legends/discrete-color-legend';
 
 export const DashboardPage = (props) => {
     const data = [{x: 0, y: 8},{x: 1, y: 5},{x: 2, y: 4}]
+    const items = [
+        { title: 'Project 1', color: '#f9c116', stroke: '#fff', strokeWidth: '2' },
+        { title: 'Project 2', color: '#006600', stroke: '#fff', strokeWidth: '2' },
+        { title: 'Project 3', color: '#1E96BE', stroke: '#fff', strokeWidth: '2' },
+        { title: 'Project 4', color: '#4c85ff', stroke: '#fff', strokeWidth: '2' },
+        { title: 'Project 5', color: '#f99416', stroke: '#fff', strokeWidth: '2' }
+    ];
     const renderRedirect = () => {
         if(!props.isAuthenticated) {
             console.log("Redirect Loaded")
@@ -30,8 +38,8 @@ export const DashboardPage = (props) => {
                 justify="center"
                 alignItems="center"
                 >
-                    <Grid item xs="6">
-                        <Typography variant="h6" align="left">Metrics by Projects</Typography>
+                    <Grid item xs="3">
+                    <Typography variant="h6" align="left">Metrics by Projects</Typography>
                         <RadialChart
                             colorType={'literal'}
                             colorDomain={[0, 100]}
@@ -39,20 +47,28 @@ export const DashboardPage = (props) => {
                             margin={{top: 100}}
                             getLabel={d => d.name}
                             data={[
-                                {angle: 1, color: '#f9c116', name: 'Project 1'},
-                                {angle: 2, color: '#006600', name: 'Project 2'},
-                                {angle: 5, color: '#1E96BE', name: 'Project 3'},
-                                {angle: 3, color: '#4c85ff', name: 'Project 4'},
-                                {angle: 5, color: '#f99416', name: 'Project 5'}
+                                {angle: 1, color: '#f9c116'},
+                                {angle: 2, color: '#006600'},
+                                {angle: 5, color: '#1E96BE'},
+                                {angle: 3, color: '#4c85ff'},
+                                {angle: 5, color: '#f99416'}
                             ]}
                             labelsRadiusMultiplier={1.1}
-                            labelsStyle={{fontSize: 16, fill: '#222'}}
+                            labelsStyle={{fontSize: 12, fill: '#222'}}
                             showLabels
                             style={{stroke: '#fff', strokeWidth: 2}}
                             width={400}
                             height={300}
                             />
                     </Grid>
+                    <Grid item xs="3">
+                        <DiscreteColorLegend
+                            height={300}
+                            width={100}
+                            items={items}
+                        />
+                    </Grid>
+
                     <Grid item xs="6">
                         <Typography variant="h6" align="left">Metrics Collected</Typography>
                         <XYPlot height={300} width={400}>
