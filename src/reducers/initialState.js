06e46553662e2d18455c72ddf6e4324446c3e6cd
Colin Kelley <kelleycd@etsu.edu>
2019-11-11 21:19:03 -0500

    Removed initial projects.

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -9,15 +9,7 @@ export default {
     error: null,
   },
   projects: {
-    data: [
-      {id: '0', name: 'Project 1', description: 'My first project'},
-      {id: '3', name: 'Project 2', description: 'My second project'},
-      {id: '8', name: 'Project 3', description: 'My third project'},
-      {id: '13', name: 'Project 4', description: 'My fourth project'},
-      {id: '55', name: 'Project 5', description: 'My fifth project'},
-      {id: '103', name: 'Project 6', description: 'My sixth project'},
-      {id: '344', name: 'Project 7', description: 'My seventh project'},
-    ],
+    data: [],
     loading: false,
     error: null,
   },
